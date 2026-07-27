4487e64974585b8aebcc6ff3bd51dc7979dade03
stanng66 <stan.ng66@gmail.com>
2026-07-27 13:11:47 -0400

    set up java file OopLab8

---
+++ OopLab8.java
@@ -0,0 +1,20 @@
+// ====================
+// Lab 8
+// Stanley Nguyen (N01570766)
+// Humber College
+// CPAN-131-RNA
+// 
+// August 3, 2026
+// --------------------
+// This program demonstrates java exception through try, catch and finally
+// --------------------
+// Assignment requirement: Implement Java Exception which we leaned it today: You must implement try ,catch and finally
+// Assignment requirement: In your main method 
+
+// Assignment requirement: create an array with 3 elements
+
+// Assignment requirement: try checking the index 5 and catch appropriate exception using catch .
+
+// Assignment requirement: Now choose any number from array and try dividing by zero and catch appropriate exception 
+
+// Assignment requirement: You must also have finally block
