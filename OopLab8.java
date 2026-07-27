12d04af53d55341b9ed9203fa70406fbfea94d35
Stanley Nguyen <stan.ng66@gmail.com>
2026-07-27 14:13:20 -0400

    Create 2 try,catch and 1 finally block

--- OopLab8.java
+++ OopLab8.java
@@ -3,18 +3,33 @@
 // Stanley Nguyen (N01570766)
 // Humber College
 // CPAN-131-RNA
-// 
+// Ronak Sheth
 // August 3, 2026
 // --------------------
 // This program demonstrates java exception through try, catch and finally
-// --------------------
-// Assignment requirement: Implement Java Exception which we leaned it today: You must implement try ,catch and finally
-// Assignment requirement: In your main method 
+// ====================
+public class OopLab8 {
+    // Assignment requirement: In your main method 
+    public static void main(String[] args) {
+        // Assignment requirement: create an array with 3 elements
+        int[] numberArray = {1, 2, 3};
 
-// Assignment requirement: create an array with 3 elements
+        // Assignment requirement: try checking the index 5 and catch appropriate exception using catch.
+        try {
+            System.out.println(numberArray[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: This index does not exist.");
+        }
 
-// Assignment requirement: try checking the index 5 and catch appropriate exception using catch .
-
-// Assignment requirement: Now choose any number from array and try dividing by zero and catch appropriate exception 
-
-// Assignment requirement: You must also have finally block
+        // Assignment requirement: Now choose any number from array and try dividing by zero and catch appropriate exception 
+        try {
+            int divideAnswer = numberArray[2] / 0;
+            System.out.println(divideAnswer);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: This index cannot be divided by zero.");
+        } finally {
+            // Assignment requirement: You must also have finally block
+            System.out.println("This is a finally block.");
+        }
+    }
+}
